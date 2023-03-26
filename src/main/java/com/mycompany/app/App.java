57e8c2be8162033096e391d3896e444c208a1975
Monika <monika01@sap.com>
2023-03-26 14:30:12 +0530

    Final commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,52 @@
+package main.java.com.mycompany.app;
+import java.util.*;
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Simple Calculator app:");
+        System.out.println("Enter your choice:\n 1 for 'Addition' \n 2 for 'Subtraction' \n 3 for 'Division' \n 4 for 'Multiplication'");
+        int n = sc.nextInt();
+        System.out.println("Enter two numbers:");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        switch(n){
+            case 1:
+            System.out.println(addition(a,b));
+            break;
+            case 2:
+            System.out.println(subtraction(a,b));
+            break;
+            case 3:
+            System.out.println(division(a,b));
+            break;
+            case 4:
+            System.out.println(multiplication(a,b));
+            break;
+            default:
+            System.out.println("Wrong choice entered");
+        }
+        sc.close();
+    }
+
+    public static double addition(double a, double b){
+        return a+b;
+    }
+
+    public static double subtraction(double a, double b){
+        return a-b;
+    }
+
+    public static String division(double a, double b){
+        if(b==0)
+            return "Division by zero not possible";
+        return String.valueOf(a/b);
+    }
+
+    public static double multiplication(double a, double b){
+        return a*b;
+    }
+}
---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,35 @@
+package test.java.com.mycompany.app;
+
+import main.java.com.mycompany.app.App;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class AppTest
+{
+
+    @Test
+    public void testAddition() {
+        assertEquals(2.0, App.addition(1,1));
+    }
+
+    @Test
+    public void testSubtraction() {
+        assertEquals(0.0, App.subtraction(1,1));
+    }
+
+    @Test
+    public void testDivision() {
+        assertEquals("2.0", App.division(2,1));
+    }
+
+    @Test
+    public void testDivisionByZero() {
+        assertEquals("Division by zero not possible", App.division(2,0));
+    }
+
+    @Test
+    public void testMultiplication() {
+        assertEquals(1.0, App.multiplication(1,1));
+    }
+}
